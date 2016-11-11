0ba545ddf9278767839325bbc53b10ef4071622b
Brandon Hinds <brandon.hinds@mymona.uwi.edu>
2016-11-11 16:34:11 +0000

    Excersize 2

---
+++ world.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    
+    $("#lookup").on('click', function(){
+        search($("#country").val());
+    });
+});
+
+function search(query){
+    var link = 'https://info2180-lab7-jasonhinds13.c9users.io/world.php?country='+query;
+    $.ajax(link,{
+        method: 'GET',
+    }).done(function(res){
+        $("#result").html(res);
+    }).fail(function(fail){
+        $("#result").html('<b>EPIC FAIL 500 - AJAX ERROR!</b>');
+    });
+}
